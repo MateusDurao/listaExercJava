201155b406747650b78d08123e08bfb8834d9df0
MateusDurao <mateusdurao06@gmail.com>
2026-03-29 18:03:19 -0300

    Exercício 3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Carro> listaCarro = new ArrayList<>();
+
+
         Carro carro1 = new Carro("tuffCars", "67", 2025);
+        listaCarro.add(carro1);
         Carro carro2 = new Carro("tuffCars", "61", 2025);
+        listaCarro.add(carro2);
+        Carro carro3 = new Carro("tuffCars", "41", 2025);
+        listaCarro.add(carro3);
 
-//        System.out.println("1º carro \n marca: " + carro1.marca + "\n modelo: " + carro1.modelo + "\n ano: " + carro1.ano);
-//        System.out.println("2º carro \n marca: " + carro2.marca + "\n modelo: " + carro2.modelo + "\n ano: " + carro2.ano);
 
-        carro1.Exibir(); // esse metodo retorna valor
-        int idade = carro1.idadeDoCarro(); // esse metodo executa uma ação
-        System.out.println("idade do carro: " + idade);
+        for(Carro c : listaCarro){
+            c.Exibir();
+        }
+
+
+
+
+//        carro1.Exibir(); // esse metodo retorna valor
+//        int idade = carro1.idadeDoCarro(); // esse metodo executa uma ação
+//        System.out.println("idade do carro: " + idade);
 
 
 
